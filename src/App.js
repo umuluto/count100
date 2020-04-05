9aa640c6a1eeae2995b429b0177a20f548d0bdbc
kooyooman <kooyooman@gmail.com>
2020-04-05 22:45:41 +0700

    first step convert html to react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,256 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var createReactClass = require('create-react-class');
+
+var NewComponent = createReactClass({
+    render: function() {
+      return (
+        <div>
+          <div style={{width: 0, height: 0, overflow: 'hidden'}}>
+            <span style={{fontFamily: '"Noto Sans" !important'}}>.</span>
+            <span style={{fontFamily: '"Noto Sans" !important', fontWeight: 'bold !important'}}>.</span>
+            <span style={{fontFamily: '"Noto Sans" !important', fontStyle: 'italic !important'}}>.</span>
+            <span style={{fontFamily: '"Noto Sans" !important', fontStyle: 'italic !important', fontWeight: 'bold !important'}}>.</span>
+            <span style={{fontFamily: '"PT Sans" !important'}}>.</span>
+            <span style={{fontFamily: '"PT Serif" !important'}}>.</span>
+            <span style={{fontFamily: '"CirceRounded" !important'}}>.</span>
+            <span style={{fontFamily: '"CirceRounded" !important', fontWeight: 'bold !important'}}>.</span>
+            <span style={{fontFamily: '"CirceRounded-Regular" !important'}}>.</span>
+            <span style={{fontFamily: '"CirceRounded-Alt-Bold" !important'}}>.</span>
+            <span style={{fontFamily: '"Circe" !important'}}>.</span>
+            <span style={{fontFamily: '"Circe" !important', fontWeight: 'bold !important'}}>.</span>
+            <span style={{fontFamily: '"Circe-Regular" !important'}}>.</span>
+            <span style={{fontFamily: '"Circe-Bold" !important'}}>.</span>
+            <span style={{fontFamily: '"DejaVu Sans" !important'}}>.</span>
+            <span style={{fontFamily: '"Neucha" !important'}}>.</span>
+          </div>
+          <div className="uchiru_bg_cell" />
+          <div className="uchiru_bg_color" />
+          <div className="uchiru_bg_stuff" />
+          <img className="uchiru_bg_color" alt="" src="https://content.dragonlearn.in/fp/97/fat_player/student/grad-f07e854ce64f9aee2db7bd67146ff0e4800feeed40c8f7116fcb3b6a048deb64.jpg" />
+          <div className="uchiru_box">
+            <div className="uchiru_head card with_progress">
+              <a className="back-link" style={{}} href="/home.html">
+                <div className="arrow-left" />
+                <span>Back</span>
+              </a>
+              <div className="uchiru-head__right-group">
+              </div>
+              <div className="beads-wrapper">
+                <div id="progress">
+                  <div className="inner_progress"><i className="az_ball_0" id="bead0" style={{left: '4px'}} /><i className="az_ball_1" id="bead1" style={{left: '28px'}} /><i className="az_ball_2" id="bead2" style={{left: '52px'}} /><i className="az_ball_3" id="bead3" style={{left: '76px'}} /><i className="az_ball_4" id="bead4" style={{left: '100px'}} /><i className="az_ball_5" id="bead5" style={{left: '124px'}} /><i className="az_ball_6" id="bead6" style={{left: '148px'}} /></div>
+                </div>
+              </div>
+            </div>
+            <div className="card_content">
+              <div id="board" className="uchiru-place card player-1 script3771 fixed run_on_windows cr" style={{lineHeight: '1.29'}}>
+                <div className="scene under_start">
+                  <div className="grid">
+                    <div className="grid_part vertical_grid">
+                      <div className="grid_column"><span className="__cnd">0</span></div>
+                      <div className="grid_column"><span className="__cnd">1</span></div>
+                      <div className="grid_column"><span className="__cnd">2</span></div>
+                      <div className="grid_column"><span className="__cnd">3</span></div>
+                      <div className="grid_column"><span className="__cnd">4</span></div>
+                      <div className="grid_column"><span className="__cnd">5</span></div>
+                      <div className="grid_column"><span className="__cnd">6</span></div>
+                      <div className="grid_column"><span className="__cnd">7</span></div>
+                      <div className="grid_column"><span className="__cnd">8</span></div>
+                      <div className="grid_column"><span className="__cnd">9</span></div>
+                      <div className="grid_column"><span className="__cnd">10</span></div>
+                      <div className="grid_column"><span className="__cnd">11</span></div>
+                      <div className="grid_column"><span className="__cnd">12</span></div>
+                      <div className="grid_column"><span className="__cnd">13</span></div>
+                      <div className="grid_column"><span className="__cnd">14</span></div>
+                      <div className="grid_column"><span className="__cnd">15</span></div>
+                      <div className="grid_column"><span className="__cnd">16</span></div>
+                      <div className="grid_column"><span className="__cnd">17</span></div>
+                      <div className="grid_column"><span className="__cnd">18</span></div>
+                      <div className="grid_column"><span className="__cnd">19</span></div>
+                      <div className="grid_column"><span className="__cnd">20</span></div>
+                      <div className="grid_column"><span className="__cnd">21</span></div>
+                      <div className="grid_column"><span className="__cnd">22</span></div>
+                      <div className="grid_column"><span className="__cnd">23</span></div>
+                      <div className="grid_column"><span className="__cnd">24</span></div>
+                      <div className="grid_column"><span className="__cnd">25</span></div>
+                      <div className="grid_column"><span className="__cnd">26</span></div>
+                      <div className="grid_column"><span className="__cnd">27</span></div>
+                      <div className="grid_column"><span className="__cnd">28</span></div>
+                      <div className="grid_column"><span className="__cnd">29</span></div>
+                      <div className="grid_column"><span className="__cnd">30</span></div>
+                      <div className="grid_column"><span className="__cnd">31</span></div>
+                      <div className="grid_column"><span className="__cnd">32</span></div>
+                      <div className="grid_column"><span className="__cnd">33</span></div>
+                      <div className="grid_column"><span className="__cnd">34</span></div>
+                      <div className="grid_column"><span className="__cnd">35</span></div>
+                      <div className="grid_column"><span className="__cnd">36</span></div>
+                      <div className="grid_column"><span className="__cnd">37</span></div>
+                      <div className="grid_column"><span className="__cnd">38</span></div>
+                      <div className="grid_column"><span className="__cnd">39</span></div>
+                      <div className="grid_column"><span className="__cnd">40</span></div>
+                      <div className="grid_column"><span className="__cnd">41</span></div>
+                      <div className="grid_column"><span className="__cnd">42</span></div>
+                      <div className="grid_column"><span className="__cnd">43</span></div>
+                      <div className="grid_column"><span className="__cnd">44</span></div>
+                      <div className="grid_column"><span className="__cnd">45</span></div>
+                      <div className="grid_column"><span className="__cnd">46</span></div>
+                      <div className="grid_column"><span className="__cnd">47</span></div>
+                    </div>
+                    <div className="grid_part horizontal_grid">
+                      <div className="grid_row" />
+                      <div className="grid_row"><span className="__cnd">1</span></div>
+                      <div className="grid_row"><span className="__cnd">2</span></div>
+                      <div className="grid_row"><span className="__cnd">3</span></div>
+                      <div className="grid_row"><span className="__cnd">4</span></div>
+                      <div className="grid_row"><span className="__cnd">5</span></div>
+                      <div className="grid_row"><span className="__cnd">6</span></div>
+                      <div className="grid_row"><span className="__cnd">7</span></div>
+                      <div className="grid_row"><span className="__cnd">8</span></div>
+                      <div className="grid_row"><span className="__cnd">9</span></div>
+                      <div className="grid_row"><span className="__cnd">10</span></div>
+                      <div className="grid_row"><span className="__cnd">11</span></div>
+                      <div className="grid_row"><span className="__cnd">12</span></div>
+                      <div className="grid_row"><span className="__cnd">13</span></div>
+                      <div className="grid_row"><span className="__cnd">14</span></div>
+                      <div className="grid_row"><span className="__cnd">15</span></div>
+                      <div className="grid_row"><span className="__cnd">16</span></div>
+                      <div className="grid_row"><span className="__cnd">17</span></div>
+                      <div className="grid_row"><span className="__cnd">18</span></div>
+                      <div className="grid_row"><span className="__cnd">19</span></div>
+                      <div className="grid_row"><span className="__cnd">20</span></div>
+                      <div className="grid_row"><span className="__cnd">21</span></div>
+                      <div className="grid_row"><span className="__cnd">22</span></div>
+                      <div className="grid_row"><span className="__cnd">23</span></div>
+                      <div className="grid_row"><span className="__cnd">24</span></div>
+                      <div className="grid_row"><span className="__cnd">25</span></div>
+                      <div className="grid_row"><span className="__cnd">26</span></div>
+                      <div className="grid_row"><span className="__cnd">27</span></div>
+                    </div>
+                  </div>
+                  <div className="task">Reducing fractions</div>
+                  <div className="sub_task">Write the number which both the numerator and the denominator can be divided
+                    by</div>
+                  <div className="action_wrap">
+                    <div className="expression_wrap" style={{width: '302px'}}>
+                      <div className="fraction">
+                        <div className="numerator">
+                          <div className="f-input blank">
+                            <div className="__outer">
+                              <div className="__inner"><span style={{width: '29px', marginTop: '-3px'}}>3</span><span style={{width: '0px', marginTop: '-3px'}} />
+                                <div className="__carriage blank" />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="dash" />
+                        <div className="denominator">
+                          <div className="f-input blank">
+                            <div className="__outer">
+                              <div className="__inner"><span style={{width: '29px', marginTop: '-3px'}}>6</span><span style={{width: '0px', marginTop: '-3px'}} />
+                                <div className="__carriage blank" />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="sign_equal">=</div>
+                      <div className="fraction">
+                        <div className="numerator">
+                          <div className="f-input blank">
+                            <div className="__outer">
+                              <div className="__inner"><span style={{width: '29px', marginTop: '-3px'}}>3</span><span style={{width: '0px', marginTop: '-3px'}} />
+                                <div className="__carriage" />
+                              </div>
+                            </div>
+                          </div><span style={{position: 'relative', top: '8px'}}>÷</span>
+                          <div className="f-input active">
+                            <div className="__outer">
+                              <div className="__inner"><span style={{width: '0px', marginTop: '-3px'}} /><span style={{width: '0px', marginTop: '-3px'}} />
+                                <div className="__carriage" />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="dash" />
+                        <div className="denominator">
+                          <div className="f-input blank">
+                            <div className="__outer">
+                              <div className="__inner"><span style={{width: '29px', marginTop: '-3px'}}>6</span><span style={{width: '0px', marginTop: '-3px'}} />
+                                <div className="__carriage" />
+                              </div>
+                            </div>
+                          </div><span style={{position: 'relative', top: '8px'}}>÷</span>
+                          <div className="f-input active">
+                            <div className="__outer">
+                              <div className="__inner"><span style={{width: '0px', marginTop: '-3px'}} /><span style={{width: '0px', marginTop: '-3px'}} />
+                                <div className="__carriage" />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="button-basic button-basic_font_ci button-basic_case_uc button-basic_color_blue button-basic_weight_bold button-basic_fixed button-ok disabled">
+                      <div className="button-basic__placeholder" style={{visibility: 'hidden'}}>
+                        <div className="button-basic__placeholder_inner ci">OK</div>
+                      </div>
+                      <div className="button-basic__wrong" />
+                      <div className="button-basic__right" />
+                      <div className="button-basic__text">
+                        <div className="button-basic__text_inner ci">OK</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="btn_play_wall" />
+                </div>
+                <div className="btn_play">
+                  <div className="triangle"><span>start</span></div>
+                </div>
+              </div>
+              <div className="keypad-iwb-2-hacker keypad-iwb-2-hacker-right keypad-iwb-2-hacker-hide">
+                <div className="keypad_buttons_holder">
+                  <div className="keypad_button keypad_button_1">1</div>
+                  <div className="keypad_button keypad_button_2">2</div>
+                  <div className="keypad_button keypad_button_3">3</div>
+                  <div className="keypad_button keypad_button_4">4</div>
+                  <div className="keypad_button keypad_button_5">5</div>
+                  <div className="keypad_button keypad_button_6">6</div>
+                  <div className="keypad_button keypad_button_7">7</div>
+                  <div className="keypad_button keypad_button_8">8</div>
+                  <div className="keypad_button keypad_button_9">9</div>
+                  <div className="keypad_button keypad_button_0">0</div>
+                  <div className="keypad_button keypad_button_plus">&nbsp;</div>
+                  <div className="keypad_button keypad_button_minus">&nbsp;</div>
+                  <div className="keypad_button keypad_button_multiply">&nbsp;</div>
+                  <div className="keypad_button keypad_button_divide">&nbsp;</div>
+                  <div className="keypad_button keypad_button_backspace">&nbsp;</div>
+                  <div className="keypad_button keypad_button_enter">OK</div>
+                </div>
+                <div className="keypad-move">&nbsp;</div>
+                <div className="keypad-showhide">Show Smartboard Keyboard</div>
+              </div>
+            </div>
+          </div>
+          <iframe id="intercom-frame" style={{position: 'absolute !important', opacity: 1, width: '1px !important', height: '1px !important', top: '0px !important', left: '0px !important', border: 'none !important', display: 'block !important', zIndex: '-1 !important', overflow: 'visible'}} aria-hidden="true" tabIndex={-1} title="Intercom" __idm_frm__={650} />
+          <div id="eJOY__extension_root" style={{all: 'unset'}} />
+          <div id="intercom-container" className="intercom-namespace">
+            <style dangerouslySetInnerHTML={{__html: "\n            html.intercom-mobile-messenger-active,\n            html.intercom-mobile-messenger-active>body,\n            html.intercom-modal-open,\n            #intercom-container-body {\n                overflow: hidden !important;\n            }\n\n            html.intercom-mobile-messenger-active,\n            html.intercom-mobile-messenger-active>body {\n                position: static !important;\n            }\n\n            html.intercom-mobile-messenger-active>body {\n                height: 0 !important;\n                margin: 0 !important;\n            }\n\n            iframe#intercom-frame {\n                position: absolute !important;\n                opacity: 0 !important;\n                width: 1px !important;\n                height: 1px !important;\n                top: 0 !important;\n                left: 0 !important;\n                border: none !important;\n                display: block !important;\n                z-index: -1 !important;\n            }\n        " }} />
+            <div className="intercom-app" aria-live="polite">
+              <div id="intercom-modal-container" />
+            </div>
+          </div>
+          <div id="intercom-css-container">
+            <style data-emotion="intercom-global" dangerouslySetInnerHTML={{__html: "" }} />
+          </div>
+        </div>
+      );
+    }
+  });
+
+class App extends React.Component {
+    render() {
+        return <NewComponent />;
+    }
 }
 
 export default App;
