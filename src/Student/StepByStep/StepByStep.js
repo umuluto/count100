7292684acd14e5994fabf0414ad2a232e3679860
ngfam <ngfamgithub@gmail.com>
2020-04-14 14:26:08 +0700

    merge 2

--- src/Student/StepByStep/StepByStep.js
+++ src/Student/StepByStep/StepByStep.js
@@ -5,11 +5,7 @@ class StepByStep extends React.Component {
     render() {
         return (
             <Home 
-<<<<<<< HEAD:src/StepByStep/StepByStep.js
-                homeScreen = {() => {this.props.homeScreen()}}
-=======
                 homeScreen = {() => this.props.mainScreen()}
->>>>>>> origin/master:src/Student/StepByStep/StepByStep.js
             />
         );
     }
