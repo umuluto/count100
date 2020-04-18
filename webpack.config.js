ac03deb33682590b3bdcefe439d7b10bc28cedc7
Minh Nguyen <ulto.mn@gmail.com>
2020-04-18 17:17:26 +0700

    Thêm 1 dependency: file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,50 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
 	entry : {
-		train: "./src/train/index.js",
-		bunny: "./src/bunny/index.js",
-		ruler: "./src/ruler/index.js"
+		train: './src/train/index.js',
+		bunny: './src/bunny/index.js',
+		ruler: './src/ruler/index.js'
 	},
-	mode: "development",
+	mode: 'development',
+	resolve: {
+		alias: {
+			Public: path.resolve(__dirname, 'public')
+		}
+	},
 	module: {
 		rules: [
 			{
 				test: /\.jsx?$/,
-				exclude: path.resolve(__dirname, "nodemodules"),
-				loader: "babel-loader",
+				exclude: path.resolve(__dirname, 'nodemodules'),
+				loader: 'babel-loader'
 			},
 			{
 				test: /\.css$/,
-				use: ["style-loader", "css-loader"]
+				use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.(png|jpe?g|gif)$/,
+				use: {
+					loader: 'file-loader',
+					options: {
+						publicPath: '/',
+						name: '[path][name].[ext]',
+						emitFile: false
+					}
+				}
 			}
+
 		]
 	},
 	output: {
 		path: path.resolve(__dirname, 'dist/'),
-		publicPath: "/dist/",
-		filename: "[name].bundle.js"
+		publicPath: '/dist/',
+		filename: '[name].bundle.js'
 	},
 	devServer: {
-		contentBase: path.join(__dirname, "public/"),
 		port: 3000,
-		publicPath: "http://localhost:3000/dist/",
-		hotOnly: true
+		publicPath: 'http://localhost:3000/dist/'
 	}
 };
