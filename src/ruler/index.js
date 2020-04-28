1f27208ae6e270cb7de87658f48a389bc94ec9c6
NIXsnowCat <thanhnam.awa@gmail.com>
2020-04-28 11:12:36 +0700

    covert box content from html to react jsx in Ruler.js

--- src/ruler/index.js
+++ src/ruler/index.js
@@ -4,10 +4,5 @@ import './style.css';
 import Ruler from './Ruler'
 
 console.log("Hello");
-//const element = <h1>Hello, world</h1>;
-function Test1 (props) {
-    return (
-        <h1 className="t">test</h1>
-    );
-}
-ReactDOM.render(<Test1></Test1>, document.getElementById('root'));
+
+ReactDOM.render(<Ruler />, document.getElementById('root'));
