f8daf0dbc153e8873f8a0d1c9281d899bc29f17f
NIXsnowCat <thanhnam.awa@gmail.com>
2020-04-28 09:30:08 +0700

    add background add background as should be seen like in the dragonlearn

--- src/ruler/index.js
+++ src/ruler/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
+import Ruler from './Ruler'
 
 console.log("Hello");
-const element = <h1>Hello, world</h1>;
-function test1 (props) {
+//const element = <h1>Hello, world</h1>;
+function Test1 (props) {
     return (
         <h1 className="t">test</h1>
     );
 }
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(<Test1></Test1>, document.getElementById('root'));
