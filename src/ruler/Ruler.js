e9495cf320ff0ed6815abb3e73a2e4acc194614c
athena <thanhnam.awa@gmail.com>
2020-05-04 00:33:54 +0700

    Thêm sự kiện khi kéo quả cầu
    Nếu vị trí là đúng, một quả cầu trên thanh trạng thái sẽ được kéo sang phải
    Nếu vị trí là sai, quả cầu gần nhất được kéo sang phải sẽ được kéo trở lại vị trí ban đầu
    Trong quá trình vẽ thước, do không căn chỉnh đúng, điều này khiến cho việc tính toán đúng sai trong sự kiện kéo thả trở nên sai lệch từ 5-10px
    Hiệu ứng chuyển giữa các trạng thái cầu vẫn chưa ổn, cần được điều chỉnh lại

--- src/ruler/Ruler.js
+++ src/ruler/Ruler.js
@@ -2,6 +2,24 @@ import React, { useState } from 'react';
 import Draggable, { DraggableCore } from 'react-draggable';
 
 function Ruler(props) {
+
+    const [progressBallIndex, setProgressBallIndex] = useState(0);
+    const [dragActive, setDragActive] = useState(0);
+    const [ballNumber, setBallNumber] = useState(63);
+    const [ballState, setBallState]   = useState(0);
+    const [defaultPosition, setDefaultPosition] = useState(
+        {x: 300, y: 0}
+    )
+    const dragHandlers = {onStart, onStop};
+    //above is const
+
+    let prBall0 = { left: "440px", transition: "all .5s ease-out",}
+    let prBall1 = { left: "416px", transition: "all .5s ease-out",}
+    let prBall2 = { left: "392px", transition: "all .5s ease-out",}
+    let prBall3 = { left: "368px", transition: "all .5s ease-out",}
+    let prBall4 = { left: "342px", transition: "all .5s ease-out",}
+    let prBall5 = { left: "318px", transition: "all .5s ease-out",}
+
     let state = {
         state_wrong: {
             backgroundImage: "url(../../public/ball_state.png)",
@@ -20,15 +38,9 @@ function Ruler(props) {
         }
     }
     let arrayBallState = [state.state_default, state.state_right, state.state_wrong];
+    //above is let
     
-    const [dragActive, setDragActive] = useState(0);
-    const [ballNumber, setBallNumber] = useState(25);
-    const [ballState, setBallState]   = useState(0);
-    const [defaultPosition, setDefaultPosition] = useState(
-        {x: 300, y: 0}
-    )
-    const dragHandlers = {onStart, onStop};
-    
+    // under is function
     function onStart () {
         let stick = document.querySelector("#stick");
         stick.style.display = "block";
@@ -37,15 +49,32 @@ function Ruler(props) {
      
     function onStop (e, position) {
         console.log(position);
-        if( (position.x > ballNumber*8-5-4) && (position.x < ballNumber*8-1) ) // because when style in css, the real value x is moved left about 5px
+        if( (position.x > (ballNumber*8-5-2)) && (position.x < (ballNumber*8+5)) ) 
+        // because when style in css, the real value x is moved left about 5px
         {    
             console.log('right position');
             setBallState(1);
+            handleRightPosition();
         }
         else{
             console.log('wrong position');
             setBallState(2);
+            handleWrongPosition();
         }
+    }
+
+    function handleRightPosition() {
+        if (progressBallIndex <= 5) setProgressBallIndex(progressBallIndex+1);
+        console.log(progressBallIndex);
+        setBallNumber(Math.floor(Math.random() * 100 ));
+    }
+
+    function handleWrongPosition() {
+        if (progressBallIndex > 0) setProgressBallIndex(progressBallIndex-1);
+        // when we decrease the progressBallIndex, the condition in className=inner_progress will be false for the last
+        // then the ball will comback to it's default position
+        console.log(ballNumber);
+        setBallNumber(Math.floor(Math.random() * 100 ));
     }
 
     return(
@@ -62,12 +91,12 @@ function Ruler(props) {
                     </a>
                     <div className="bar_wrapper">                        
                             <div className="inner_progress">
-                                <i id="ball0" className="rotate_ball_0" style={{left: "4px"}}></i>
-                                <i id="ball1" className="rotate_ball_1" style={{left: "28px"}}></i>
-                                <i id="ball2" className="rotate_ball_2" style={{left: "52px"}}></i>
-                                <i id="ball3" className="rotate_ball_3" style={{left: "76px"}}></i>
-                                <i id="ball4" className="rotate_ball_4" style={{left: "100px"}}></i>
-                                <i id="ball5" className="rotate_ball_5" style={{left: "124px"}}></i>                            
+                                <i id="ball0" className="rotate_ball_0" style={ progressBallIndex > 5 ? prBall5 : {left: "4px"}}></i>
+                                <i id="ball1" className="rotate_ball_1" style={ progressBallIndex > 4 ? prBall4 : {left: "28px"}}></i>
+                                <i id="ball2" className="rotate_ball_2" style={ progressBallIndex > 3 ? prBall3 : {left: "52px"}}></i>
+                                <i id="ball3" className="rotate_ball_3" style={ progressBallIndex > 2 ? prBall2 : {left: "76px"}}></i>
+                                <i id="ball4" className="rotate_ball_4" style={ progressBallIndex > 1 ? prBall1 : {left: "100px"}}></i>
+                                <i id="ball5" className="rotate_ball_5" style={ progressBallIndex > 0 ? prBall0 : {left: "124px"}}></i>                            
                             </div>
                     </div>
                 </div>
@@ -84,7 +113,7 @@ function Ruler(props) {
                                             <div className="stick" id="stick" style={{display: "none"}}>
                                                 <div className="point"></div>
                                             </div>
-                                            <span>25</span>
+                                            <span>{ballNumber}</span>
                                         </div>
                                     </Draggable>
                                 </div>
