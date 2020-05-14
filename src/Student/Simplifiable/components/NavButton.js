d087cc78372d7be7709199ea4fcccb1b6f2acdf2
Le Duy <leqduyvp@gmail.com>
2020-05-14 11:49:00 +0700

    fix: insert left arrow into back button

--- src/Student/Simplifiable/components/NavButton.js
+++ src/Student/Simplifiable/components/NavButton.js
@@ -18,14 +18,47 @@ function NavButton(props) {
     position: "relative",
   };
 
+  const arrowStyle = {
+    top: "-2px",
+    verticalAlign: "middle",
+    display: "inline-block",
+    width: 0,
+    height: 0,
+    borderTop: "15px solid transparent",
+    borderBottom: "15px solid transparent",
+    borderRight: "15px solid #246EA1",
+    position: "relative",
+    borderRadius: "50%",
+  };
+
+  const beforeArrow = {
+    content: "",
+    position: "absolute",
+    width: 0,
+    height: 0,
+    right: "-18px",
+    top: "-15px",
+    borderTop: "15px solid transparent",
+    borderBottom: "15px solid transparent",
+    borderRight: "15px solid white",
+  };
+
   return (
-    <div>
-      <a
+    <div className="glow">
+      {/* <a
         className="back-link pointer"
         onClick={props.mainScreen}
         style={backButtonStyle}
       >
+        <div class="arrow-left">::before</div>
         <span> Trở lại màn hình chính</span>
+      </a> */}
+
+      <a class="pointer" style={backButtonStyle} onClick={props.mainScreen}>
+        <div style={arrowStyle}>
+          <div style={beforeArrow}></div>
+        </div>
+        <span className=" no-underline"> Trở lại màn hình chính</span>
       </a>
     </div>
   );
