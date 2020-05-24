3350a1af183dc67b7516897c5d8dece279fe8872
cuongjuno <50716746+cuongjuno@users.noreply.github.com>
2020-05-24 23:39:06 +0700

    Cài đặt mạch game

--- src/bunny/index.js
+++ src/bunny/index.js
@@ -1,1 +1,31 @@
-console.log("webpack rocks!");
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import Pantry from './Pantry';
+import Table from './Table';
+import Bunny from './Bunny'
+import './temp.css';
+
+function App() {
+    const [game, setGame] = useState(1); // set màn chơi
+    const [isDone, setisDone] = useState(false); // đã hoàn thành màn chơi chưa
+
+
+    return (
+        <div className='board' >
+            <div className="all_wrapper" style={{ width: '1536px' }}>
+                <div className="scene_wrapper">
+                    <Table game={game} isDone={isDone} setisDone={setisDone} />
+
+                    <div className='tree1' />
+                    <div className='tree2' />
+                    
+                    <Pantry />
+                    <Bunny game={game} isDone={isDone} setisDone={setisDone} setGame={setGame} />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(< App />, rootElement)
